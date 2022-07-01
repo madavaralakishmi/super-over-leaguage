4bbbb0bed3791a3bd105ff51733e18271898ea06
varalakshmi <varalakshmi@77.com>
2022-07-01 08:31:17 +0000

    react js

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg_color">
+    <h1 className="headingColor">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img1"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img1"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
